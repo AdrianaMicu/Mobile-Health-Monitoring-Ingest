ac70cffe911ad684475ee62ad4a1438f2f1ecb7a
adrianamicuibm <adriana.micu@ro.ibm.com>
2015-02-15 14:31:09 +0200

    Initial Commit

---
+++ MobileHealthMonitoring/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MobileHealthMonitoring
+//
+//  Created by Adriana Micu on 15/02/15.
+//  Copyright (c) 2015 Adriana Micu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
